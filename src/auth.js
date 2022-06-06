137b577297759c65b71cff43404f72fa75a1427c
Anshul Trivedi <z5359068@ad.unsw.edu.au>
2022-06-06 16:59:00 +1000

    Spelling correction for function stubs in authRegisterV1 and authLoginV1

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,9 @@
-// Returns a string concatinatiaon of the input arguments 'email' and 'password 
+// Returns a string concatination of the input arguments 'email' and 'password 
 function authLoginV1(email, password) {
     return 'email' + 'password';
 }
 
-// Returns a string concatinatiaon of the input arguments 'email', 'password', 'nameFirst' and 'nameLast'
+// Returns a string concatination of the input arguments 'email', 'password', 'nameFirst' and 'nameLast'
 function authRegisterV1(email, password, nameFirst, nameLast) {
     return 'email' + 'password' + 'namefirst' + 'namelast';
 }

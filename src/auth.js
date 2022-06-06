75b38ba812bd5c7b6d8ec7c72ab83cf96421787e
Anshul Trivedi <z5359068@ad.unsw.edu.au>
2022-06-06 16:57:25 +1000

    Added stub code for function authRegisterV1

--- src/auth.js
+++ src/auth.js
@@ -2,3 +2,8 @@
 function authLoginV1(email, password) {
     return 'email' + 'password';
 }
+
+// Returns a string concatinatiaon of the input arguments 'email', 'password', 'nameFirst' and 'nameLast'
+function authRegisterV1(email, password, nameFirst, nameLast) {
+    return 'email' + 'password' + 'namefirst' + 'namelast';
+}

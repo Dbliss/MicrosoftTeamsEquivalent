27c7c15ffde9fc6ce098c89bf4fa2d5f9f27ed8d
Anshul Trivedi <z5359068@ad.unsw.edu.au>
2022-06-08 19:17:38 +1000

    Deleting function authRegisterV1 so we merge one function at a time

--- src/auth.js
+++ src/auth.js
@@ -3,7 +3,3 @@ function authLoginV1(email, password) {
     return 'email' + 'password';
 }
 
-// Returns a string concatination of the input arguments 'email', 'password', 'nameFirst' and 'nameLast'
-function authRegisterV1(email, password, nameFirst, nameLast) {
-    return 'email' + 'password' + 'namefirst' + 'namelast';
-}

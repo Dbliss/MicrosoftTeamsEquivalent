b73a621e8e6835ad7975d53709886c0b73208563
Anshul Trivedi <z5359068@ad.unsw.edu.au>
2022-06-08 20:11:09 +1000

    Edited version of the authRegisterV1 fnction stub of the return value

--- src/auth.js
+++ src/auth.js
@@ -5,5 +5,5 @@ function authLoginV1(email, password) {
 
 // Returns a string concatination of the input arguments 'email', 'password', 'nameFirst' and 'nameLast'
 function authRegisterV1(email, password, nameFirst, nameLast) {
-    return 'email' + 'password' + 'namefirst' + 'namelast';
+    return 'email' + 'password' + 'nameFirst' + 'nameLast';
 }

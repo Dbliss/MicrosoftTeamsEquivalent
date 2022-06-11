5b9cc4c40d4a7bb7f6dcbb6e1b38a9d469c7991b
Giuliana De Bellis <z5309331@unsw.edu.au>
2022-06-11 16:19:23 +1000

    Results push

--- src/auth.js
+++
@@ -1,9 +0,0 @@
-// Returns a string concatination of the input arguments 'email' and 'password 
-function authLoginV1(email, password) {
-    return 'email' + 'password';
-}
-
-// Returns a string concatination of the input arguments 'email', 'password', 'nameFirst' and 'nameLast'
-function authRegisterV1(email, password, nameFirst, nameLast) {
-    return 'email' + 'password' + 'nameFirst' + 'nameLast';
-}

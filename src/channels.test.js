cb4cb9f4664da105729dd324993bc068b1f09405
Shiv Patel <z5362167@ad.unsw.edu.au>
2022-06-20 16:49:51 +1000

    Completed all tests and added an assumption regarding channels functions

--- src/channels.test.js
+++ src/channels.test.js
@@ -60,17 +60,210 @@ describe('Testing channelsCreateV1', () => {
 
 describe('Testing channelsListV1', () => {
 
-  test('', () => {
+  test('Listing 2 channels user is part of', () => {
     clearV1();
+    let authUserId = authRegisterV1('email@email.com', 
+                                    'password', 
+                                    'First',
+                                    'Last');
+                                    
+    let channelId1 = channelsCreateV1(authUserId, 
+                                  'name1', 
+                                  false);
+                  
+    let channelId2 = channelsCreateV1(authUserId, 
+                                  'name2', 
+                                  true);
+                                  
+    let result = channelsListV1(authUserId);
+    expect(result).toEqual([{
+                              channelId: channelId1,
+                              name: 'name1'},
+                            {
+                              channelId: channelId2,
+                              name: 'name2'}]);
+                                  
   });
+  
+  test('Listing 1 channel user is part of', () => {
+    clearV1();
+    let authUserId1 = authRegisterV1('email@email.com', 
+                                     'password', 
+                                     'First',
+                                     'Last');
+    
+    let authUserId2 = authRegisterV1('email2@email.com', 
+                                     'password2', 
+                                     'First2',
+                                     'Last2');
+                         
+                                    
+    let channelId1 = channelsCreateV1(authUserId1, 
+                                  'name1', 
+                                  false);
+                  
+    let channelId2 = channelsCreateV1(authUserId2, 
+                                  'name2', 
+                                  true);
+                                  
+    let result = channelsListV1(authUserId2);
+    expect(result).toEqual([{
+                                  channelId: channelId2,
+                                  name: 'name2'}]);
+                                  
+  });
+  
+  test('Listing 0 channels user is not part of any', () => {
+    clearV1();
+    let authUserId1 = authRegisterV1('email@email.com', 
+                                     'password', 
+                                     'First',
+                                     'Last');
+    
+    let authUserId2 = authRegisterV1('email2@email.com', 
+                                     'password2', 
+                                     'First2',
+                                     'Last2');
+                         
+                                    
+    let channelId1 = channelsCreateV1(authUserId1, 
+                                  'name1', 
+                                  false);
+                  
+    let channelId2 = channelsCreateV1(authUserId1, 
+                                  'name2', 
+                                  true);
+                                  
+    let result = channelsListV1(authUserId2);
+    expect(result).toEqual([]);
+                                  
+  });
+  
+  test('authUserId Invalid', () => {
+    clearV1();
+    let authUserId1 = authRegisterV1('email@email.com', 
+                                     'password', 
+                                     'First',
+                                     'Last');
+    
+    let channelId1 = channelsCreateV1(authUserId1, 
+                                  'name1', 
+                                  false);
+                                  
+    let result = channelsListV1(-99999);
+    expect(result).toEqual([]);
+                                  
+  });
+  
+  
 
 });
 
 describe('Testing channelsListallV1', () => {
 
-  test('', () => {
+  test('Listing 2 channels created by user', () => {
     clearV1();
+    let authUserId = authRegisterV1('email@email.com', 
+                                    'password', 
+                                    'First',
+                                    'Last');
+                                    
+    let channelId1 = channelsCreateV1(authUserId, 
+                                  'name1', 
+                                  false);
+                  
+    let channelId2 = channelsCreateV1(authUserId, 
+                                  'name2', 
+                                  true);
+                                  
+    let result = channelsListAllV1(authUserId);
+    expect(result).toEqual([{
+                              channelId: channelId1,
+                              name: 'name1'},
+                            {
+                              channelId: channelId2,
+                              name: 'name2'}]);
+                                  
   });
+  
+  test('Listing 2 channels user created 1', () => {
+    clearV1();
+    let authUserId1 = authRegisterV1('email@email.com', 
+                                     'password', 
+                                     'First',
+                                     'Last');
+    
+    let authUserId2 = authRegisterV1('email2@email.com', 
+                                     'password2', 
+                                     'First2',
+                                     'Last2');
+                         
+                                    
+    let channelId1 = channelsCreateV1(authUserId1, 
+                                  'name1', 
+                                  false);
+                  
+    let channelId2 = channelsCreateV1(authUserId2, 
+                                  'name2', 
+                                  true);
+                                  
+    let result = channelsListAllV1(authUserId2);
+    expect(result).toEqual([{
+                              channelId: channelId1,
+                              name: 'name1'},
+                            {
+                              channelId: channelId2,
+                              name: 'name2'}]);
+                                  
+  });
+  
+    test('Listing 2 channels user created 0', () => {
+    clearV1();
+    let authUserId1 = authRegisterV1('email@email.com', 
+                                     'password', 
+                                     'First',
+                                     'Last');
+    
+    let authUserId2 = authRegisterV1('email2@email.com', 
+                                     'password2', 
+                                     'First2',
+                                     'Last2');
+                         
+                                    
+    let channelId1 = channelsCreateV1(authUserId1, 
+                                  'name1', 
+                                  false);
+                  
+    let channelId2 = channelsCreateV1(authUserId1, 
+                                  'name2', 
+                                  true);
+                                  
+    let result = channelsListAllV1(authUserId2);
+    expect(result).toEqual([{
+                              channelId: channelId1,
+                              name: 'name1'},
+                            {
+                              channelId: channelId2,
+                              name: 'name2'}]);
+                                  
+  });
+  
+  test('authUserId Invalid', () => {
+    clearV1();
+    let authUserId1 = authRegisterV1('email@email.com', 
+                                     'password', 
+                                     'First',
+                                     'Last');
+    
+    let channelId1 = channelsCreateV1(authUserId1, 
+                                  'name1', 
+                                  false);
+                                  
+    let result = channelsListAllV1(-99999);
+    expect(result).toEqual([]);
+                                  
+  });
+  
 
 });
 

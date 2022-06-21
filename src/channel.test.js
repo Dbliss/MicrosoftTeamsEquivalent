1b23e3c3fee18cda95f37af44a179a4f00f8da45
Dillon Bliss-Henaghan <z5259408@ad.unsw.edu.au>
2022-06-21 18:11:10 +1000

    added tests for function channelMessagesV1

--- src/channel.test.js
+++ src/channel.test.js
@@ -5,7 +5,7 @@ import {
     channelJoinV1,
     channelInviteV1,
     channelMessagesV1, 
-} from '.channel.js';
+} from './channel.js';
 
 // importing other essential functions used in channel
 
@@ -16,7 +16,7 @@ import {
 
 import {
     authRegisterV1,
-  } from "auth.js";
+  } from "./auth.js";
   
   import {
     clearV1,
@@ -97,8 +97,72 @@ import {
         
         expect(result).toMatchObject({});
     });
-    
-  
-  
   });
+  
+
+  describe('Testing channelMessages1', () => {
+  
+    test('channelId does not refer to a valid channel', () => {
+        clearV1();
+        let authUserId = authRegisterV1('email@email.com', 
+                                      'password', 
+                                      'First',
+                                      'Last');
+                                      
+        let result = channelMessagesV1(authUserId, 1, 0);
+        
+        expect(result).toMatchObject({error: 'error'});
+    });
+
+    test('start is greater than the total number of messages in the channel', () => {
+        clearV1();
+        let authUserId = authRegisterV1('email@email.com', 
+                                      'password', 
+                                      'First',
+                                      'Last');
+
+        let channelId = channelsCreateV1(authUserId, 'name', false);
+                                      
+        let result = channelMessagesV1(authUserId, channelId, 999999);
+        
+        expect(result).toMatchObject({error: 'error'});
+    });
+
+    test('ChannelId is valid and the authorised user is not a member of the channel', () => {
+        clearV1();
+        let authUserId = authRegisterV1('email@email.com', 
+                                      'password', 
+                                      'First',
+                                      'Last');
+       
+        let authUserId2 = authRegisterV1('email2@email.com', 
+                                      'password2', 
+                                      'First2',
+                                      'Last2');
+         
+        let channelId = channelsCreateV1(authUserId, 'name', false);
+                                      
+        let result = channelMessagesV1(authUserId2, channelId, 0);
+        
+        expect(result).toMatchObject({error: 'error'});
+    });
+    
+    test('no errors', () => {
+        clearV1();
+        let authUserId = authRegisterV1('email@email.com', 
+                                      'password', 
+                                      'First',
+                                      'Last');
+         
+        let channelId = channelsCreateV1(authUserId, 'name', false);
+                                      
+        let result = channelMessagesV1(authUserId, channelId, 0);
+        
+        expect(result).toEqual({
+                                    messages: '',
+                                    start: 0,
+                                    end: -1});
+    });
+  });
+
   

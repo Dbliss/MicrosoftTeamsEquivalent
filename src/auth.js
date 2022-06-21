9469132f8f1e3e0dede03dda0589c783a30f3ce8
Thevindu Hewapathiranage <z5364121@ad.unsw.edu.au>
2022-06-21 13:35:01 +1000

    added email duplicate helper function

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,14 @@
 import { getData, setData } from './dataStore';
 
+function isDuplicateEmail(email) { 
+    for (const user of getData().users) {
+        if (user.email === email) {
+            return true;
+        } 
+    }
+    return false;
+}
+
 // Returns a string concatination of the input arguments 'email', 'password', 'nameFirst' and 'nameLast'
 function authRegisterV1(email, password, nameFirst, nameLast) {
     // test for valid email i.e whether it has @ or .
@@ -7,41 +16,34 @@ function authRegisterV1(email, password, nameFirst, nameLast) {
     if (emailTest.includes('@') != true || emailTest.includes('.') != true) {
         return { error: 'error' };
     } else {
-        const data = getData();
         // test for duplicate emails in database
-        if ((email in data.users)) {
+        if (isDuplicateEmail(email) === true) {
             return { error: 'error' };
         } else {
              // see if password length is less than 6 characters
-            const passwordString = JSON.stringify(password);
-            const passwordLength = passwordString.length;
-            if (passwordLength < 6) {
+            if (password.length < 6) {
                 return { error: 'error' };
             } else {
-                const firstnameString = JSON.stringify(nameFirst);
-                const firstnameLength = firstnameString.length;
-                if (firstnameLength < 1 || firstnameLength > 50) {
+                if (nameFirst.length < 1 || nameFirst.length > 50) {
                     return { error: 'error' };
                 } else {
-                    const lastnameString = JSON.stringify(nameLast);
-                    const lastnameLength = lastnameString.length;
-                    if (lastnameLength < 1 || lastnameLength > 50) {
+                    if (nameLast.length < 1 || nameLast.length > 50) {
                         return { error: 'error' };
                     } else {
                         // create userId by concatenating nameFirst and nameLast and making it lowercase
-                        const data = getData();
                         let handleString = JSON.stringify(nameFirst);
                         handleString += JSON.stringify(nameLast);
                         let handleName = handleString.toLowerCase();
                         // add for loop for taken handles
                         let i = 0;
-                        for (const handle in data.users) {
-                            if (handleName === handle) {
+                        for (const user of getData().users) {
+                            if (user.handle === handleName) {
                                 handleName = handleName + i;
                                 i++;
-                            }
+                            } 
                         }
                         const uID = Date.now(); 
+                        const data = getData();
                         data.users[email] = {
                             name: '${nameFirst} ${nameLast}',
                             password: password,
--- src/auth.test.js
+++ src/auth.test.js
@@ -1,5 +1,5 @@
-import { authLoginV1, authRegisterV1 } from './auth';
-import { clearV1 } from './other'
+import { authLoginV1, authRegisterV1 } from './auth.js';
+import { clearV1 } from './other.js'
 
 test('Test successful returns authUserId', () => {
     clearV1();

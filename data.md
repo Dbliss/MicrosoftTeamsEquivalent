8af744b5bfa3bf7183995dd8e94500730b8659c7
Anshul Trivedi <z5359068@ad.unsw.edu.au>
2022-06-22 21:10:44 +1000

    Added a few changes such as export and some sanity checks for auth.js and data.md respectively

--- data.md
+++ data.md
@@ -23,6 +23,7 @@ database = {
             'isPublic': Channel1publicity,
             'start': Channel1start,
             'members': [ uIDs ]
+            'ownerMembers': [ memberuIds ]
         },
     ]
 }
--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,9 @@
 // Returns a string concatination of the input arguments 'email' and 'password 
  export function authLoginV1(email, password) {
+    console.log();
     return 'email' + 'password';
 }
 
 // Returns a string concatination of the input arguments 'email', 'password', 'nameFirst' and 'nameLast'
 export function authRegisterV1(email, password, nameFirst, nameLast) {
     return 'email' + 'password' + 'nameFirst' + 'nameLast';
-}

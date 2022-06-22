b8a5db305e3919b1df5ede1d87a05d2e38ec6f29
Dillon Bliss-Henaghan <d.blisshenaghan@student.unsw.edu.au>
2022-06-22 13:53:28 +1000

    Revert "Merge branch 'auth_register' into 'master'"
    This reverts merge request !21

--- src/auth.js
+++ src/auth.js
@@ -1,62 +1,9 @@
-import { getData, setData } from './dataStore';
-
-
+// Returns a string concatination of the input arguments 'email' and 'password 
+function authLoginV1(email, password) {
+    return 'email' + 'password';
+}
 
 // Returns a string concatination of the input arguments 'email', 'password', 'nameFirst' and 'nameLast'
 function authRegisterV1(email, password, nameFirst, nameLast) {
-    // test for valid email i.e whether it has @ or .
-    const emailTest = JSON.stringify(email);
-    if (emailTest.includes('@') != true || emailTest.includes('.') != true) {
-        return { error: 'error' };
-    } else {
-        // test for duplicate emails in database
-        const data = getData();
-        if (email in data.users) {
-            return { error: 'error' };
-        } else {
-             // see if password length is less than 6 characters
-            if (password.length < 6) {
-                return { error: 'error' };
-            } else {
-                if (nameFirst.length < 1 || nameFirst.length > 50) {
-                    return { error: 'error' };
-                } else {
-                    if (nameLast.length < 1 || nameLast.length > 50) {
-                        return { error: 'error' };
-                    } else {
-                        // create userId by concatenating nameFirst and nameLast and making it lowercase
-                        let handleString = JSON.stringify(nameFirst);
-                        handleString += JSON.stringify(nameLast);
-                        let handleName = handleString.toLowerCase();
-                        // add for loop for taken handles
-                        let i = 0;
-                        const data = getData();
-                        for (const user of data.users) {
-                            if (handleName === data.users[handle]) {
-                                handleName = handleName + i;
-                                i++;
-                            } 
-                        }
-                        const uID = Date.now(); 
-                        data.users[email] = {
-                            name: '${nameFirst} ${nameLast}',
-                            password: password,
-                            handle: handleName,
-                            userID: uID,
-                        }
-                        setData(data);
-                        return {
-                            authUserId: uID,
-                        }
-                    }
-                }
-            }
-        } 
-    }
+    return 'email' + 'password' + 'nameFirst' + 'nameLast';
 }
-// Returns a string concatination of the input arguments 'email' and 'password 
-function authLoginV1(email, password) {
-
-    return 'email' + 'password';
-}
-export { authRegisterV1, authLoginV1 }
--- src/auth.test.js
+++ src/auth.test.js
@@ -1,14 +1,13 @@
-import { authLoginV1, authRegisterV1 } from './auth.js';
-import { clearV1 } from './other.js'
+import { authLoginV1, authRegisterV1 } from './auth';
 
 test('Test successful returns authUserId', () => {
-    clearV1();
+    clear();
     let result = authRegisterV1('z5364121@unsw.edu.au', 'banana', 'Thevindu', 'Hewa');
-    expect(result).toMatchObject({ authUserId: expect.any(Number)});
+    expect(result).toBe('thevinduhewa');
 });
 
 test('Invalid email', () => {
-    clearV1();
+    clear();
     let result = authRegisterV1('z5364121unsw.edu.au', 'banana', 'Thevindu', 'Hewa');
     expect(result).toMatchObject({ error: 'error' });
     result = authRegisterV1('z5364121@unsweduau', 'banana', 'Thevindu', 'Hewa');
@@ -16,20 +15,20 @@ test('Invalid email', () => {
 });
 
 test('error when email is being used by another user', () => {
-    clearV1();
+    clear();
     authRegisterV1('z5364121@unsw.edu.au', 'banana', 'Thevindu', 'Hewa');
     let result = authRegisterV1('z5364121@unsw.edu.au', 'banana', 'Thev', 'Hewa');
     expect(result).toMatchObject({ error: 'error' });
 });
 
 test('password length less than 6 characters', () => {
-    clearV1();
+    clear();
     let result = authRegisterV1('z5364121@unsw.edu.au', 'apple', 'Thev', 'Hewa');
     expect(result).toMatchObject({ error: 'error' });
 });
 
 test('length of nameFirst is not between 1 and 50 characters inclusive', () => {
-    clearV1();
+    clear();
     let result = authRegisterV1('z5364121@unsw.edu.au', 'apple', '', 'Hewa');
     expect(result).toMatchObject({ error: 'error' });
     result = authRegisterV1('z5364121@unsw.edu.au', 'apple', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz', 'Hewa');
@@ -37,7 +36,7 @@ test('length of nameFirst is not between 1 and 50 characters inclusive', () => {
 });
 
 test('length of nameLast is not between 1 and 50 characters inclusive', () => {
-    clearV1();
+    clear();
     let result = authRegisterV1('z5364121@unsw.edu.au', 'apple', 'Thev', '');
     expect(result).toMatchObject({ error: 'error' });
     result = authRegisterV1('z5364121@unsw.edu.au', 'apple', 'Thev', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz');

78efefa2c176209dee2f9f100f8e5ab3a2005475
Thevindu Hewapathiranage <z5364121@ad.unsw.edu.au>
2022-07-11 15:43:41 +1000

    Finished http wrapping on iteration 1 auth functions

--- assumptions.md
+++ assumptions.md
@@ -11,3 +11,7 @@ Assumptions :
 5. A valid channelpermissionId is either a 1 for channel owner permissions or a 2 for member permissions
 
 6. Once a global owner is invited through channelInviteV1, they are treated as a member until further action is taken by channel owner
+
+7. Token is a combination of letters and numbers randomly generated such that is a lesser chance of repetition 
+
+8. Token is an array of string to store multiple tokens for each user session 
--- src/auth.test.ts
+++ src/auth.test.ts
@@ -1,65 +1,258 @@
-import { authLoginV1, authRegisterV1 } from './auth';
-import { clearV1 } from './other';
+import request from 'sync-request';
+import config from './config.json';
 
-test('Test successful returns authUserId', () => {
-  clearV1();
-  const result = authRegisterV1('z5364121@unsw.edu.au', 'banana', 'Thevindu', 'Hewa');
-  expect(result).toMatchObject({ authUserId: expect.any(Number) });
+const OK = 200;
+const port = config.port;
+const url = config.url;
+
+const del = (path, qs) => {
+  request(
+    'DELETE',
+    `${url}:${port}/${path}`,
+    {
+      qs: qs
+    }
+  );
+};
+
+describe('Test auth/register/v2', () => {
+  test('Successfully returns authUserId', () => {
+    del('clear/v1', '');
+    const res = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123', nameFirst: 'first', nameLast: 'last'
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(OK);
+    expect(bodyObj).toMatchObject({ token: expect.any(String), authUserId: expect.any(Number) });
+  });
+
+  test('Invalid email', () => {
+    del('clear/v1', '');
+    const res = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'emailemail.com', password: 'password123', nameFirst: 'first', nameLast: 'last'
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(OK);
+    expect(bodyObj).toMatchObject({ error: 'error' });
+  });
+
+  test('email taken by another user', () => {
+    del('clear/v1', '');
+    const res = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123', nameFirst: 'first', nameLast: 'last'
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    expect(res.statusCode).toBe(OK);
+    const res1 = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'emailemail.com', password: 'password123', nameFirst: 'first1', nameLast: 'last1'
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj = JSON.parse(res1.body as string);
+    expect(res1.statusCode).toBe(OK);
+    expect(bodyObj).toMatchObject({ error: 'error' });
+  });
+
+  test('password length less than 6 characters', () => {
+    del('clear/v1', '');
+    const res = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'pass', nameFirst: 'first', nameLast: 'last'
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(OK);
+    expect(bodyObj).toMatchObject({ error: 'error' });
+  });
+
+  test('length of first name not between 1 and 50 characters inclusive', () => {
+    del('clear/v1', '');
+    const res = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123', nameFirst: '', nameLast: 'last'
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(OK);
+    expect(bodyObj).toMatchObject({ error: 'error' });
+    const res1 = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123', nameFirst: 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz', nameLast: 'last'
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj1 = JSON.parse(res1.body as string);
+    expect(res1.statusCode).toBe(OK);
+    expect(bodyObj1).toMatchObject({ error: 'error' });
+  });
+
+  test('length of last name not between 1 and 50 characters inclusive', () => {
+    del('clear/v1', '');
+    const res = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123', nameFirst: 'first', nameLast: ''
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(OK);
+    expect(bodyObj).toMatchObject({ error: 'error' });
+    const res1 = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123', nameFirst: 'first', nameLast: 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj1 = JSON.parse(res1.body as string);
+    expect(res1.statusCode).toBe(OK);
+    expect(bodyObj1).toMatchObject({ error: 'error' });
+  });
 });
 
-test('Invalid email', () => {
-  clearV1();
-  let result = authRegisterV1('z5364121unsw.edu.au', 'banana', 'Thevindu', 'Hewa');
-  expect(result).toMatchObject({ error: 'error' });
-  result = authRegisterV1('z5364121@unsweduau', 'banana', 'Thevindu', 'Hewa');
-  expect(result).toMatchObject({ error: 'error' });
-});
-
-test('error when email is being used by another user', () => {
-  clearV1();
-  authRegisterV1('z5364121@unsw.edu.au', 'banana', 'Thevindu', 'Hewa');
-  const result = authRegisterV1('z5364121@unsw.edu.au', 'banana', 'Thev', 'Hewa');
-  expect(result).toMatchObject({ error: 'error' });
-});
-
-test('password length less than 6 characters', () => {
-  clearV1();
-  const result = authRegisterV1('z5364121@unsw.edu.au', 'apple', 'Thev', 'Hewa');
-  expect(result).toMatchObject({ error: 'error' });
-});
-
-test('length of nameFirst is not between 1 and 50 characters inclusive', () => {
-  clearV1();
-  let result = authRegisterV1('z5364121@unsw.edu.au', 'apple', '', 'Hewa');
-  expect(result).toMatchObject({ error: 'error' });
-  result = authRegisterV1('z5364121@unsw.edu.au', 'apple', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz', 'Hewa');
-  expect(result).toMatchObject({ error: 'error' });
-});
-
-test('length of nameLast is not between 1 and 50 characters inclusive', () => {
-  clearV1();
-  let result = authRegisterV1('z5364121@unsw.edu.au', 'apple', 'Thev', '');
-  expect(result).toMatchObject({ error: 'error' });
-  result = authRegisterV1('z5364121@unsw.edu.au', 'apple', 'Thev', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz');
-  expect(result).toMatchObject({ error: 'error' });
-});
-
-test('Login succesful', () => {
-  clearV1();
-  authRegisterV1('z5364121@unsw.edu.au', 'banana', 'Thevindu', 'Hewa');
-  const result = authLoginV1('z5364121@unsw.edu.au', 'banana');
-  expect(result).toMatchObject({ authUserId: expect.any(Number) });
-});
-
-test('email does not belong to any user', () => {
-  clearV1();
-  const result = authLoginV1('z5364121@unsw.edu.au', 'apple');
-  expect(result).toMatchObject({ error: 'error' });
-});
-
-test('password is not correct', () => {
-  clearV1();
-  authRegisterV1('z5364121@unsw.edu.au', 'banana', 'Thevindu', 'Hewa');
-  const result = authLoginV1('z5364121@unsw.edu.au', 'apple');
-  expect(result).toMatchObject({ error: 'error' });
+describe('Test auth/login/v2', () => {
+  test('Login successful', () => {
+    del('clear/v1', '');
+    const res = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123', nameFirst: 'first', nameLast: 'last',
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    expect(res.statusCode).toBe(OK);
+    const res1 = request(
+      'POST',
+      `${url}:${port}/auth/login/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123',
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res1.statusCode).toBe(OK);
+    expect(bodyObj).toMatchObject({ token: expect.any(String), authUserId: expect.any(Number) });
+  });
+  test('email does not belong to any user', () => {
+    del('clear/v1', '');
+    const res = request(
+      'POST',
+      `${url}:${port}/auth/login/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123',
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj = JSON.parse(res.body as string);
+    expect(res.statusCode).toBe(OK);
+    expect(bodyObj).toMatchObject({ error: 'error' });
+  });
+  test('password is not correct', () => {
+    del('clear/v1', '');
+    const res = request(
+      'POST',
+      `${url}:${port}/auth/register/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: 'password123', nameFirst: 'first', nameLast: 'last',
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    expect(res.statusCode).toBe(OK);
+    const res1 = request(
+      'POST',
+      `${url}:${port}/auth/login/v2`,
+      {
+        body: JSON.stringify({
+          email: 'email@email.com', password: '',
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        },
+      }
+    );
+    const bodyObj = JSON.parse(res1.body as string);
+    expect(res1.statusCode).toBe(OK);
+    expect(bodyObj).toMatchObject({ error: 'error' });
+  });
 });

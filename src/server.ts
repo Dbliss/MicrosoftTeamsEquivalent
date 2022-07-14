5443e65344707b2972dde5e517ca9c3c970b0c8f
Anshul Trivedi <z5359068@ad.unsw.edu.au>
2022-07-15 01:56:53 +1000

    Added the HTTP wrappers for the channelDetailsV1 and channelJoinV1 and all of the users functions for iteration 2

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ import cors from 'cors';
 import { authLoginV1, authRegisterV1, authLogoutV1 } from './auth';
 import { clearV1 } from './other';
 
+import { channelDetailsV1, channelJoinV1 } from './channel';
+import { usersAllV1, userProfileSetNameV1, userProfileSetEmailV1, userProfileSetHandleV1, userProfileV1} from './users';
+
+
 import {
   channelsCreateV1,
   channelsListV1,
@@ -81,6 +85,77 @@ app.post('/auth/logout/v1', (req, res, next) => {
     next(err);
   }
 });
+
+//WRAPPING CHANNEL FUNCTIONS - channelDetailsV1 (GET) and channelJoinV1 (POST)
+
+app.get('channel/details/v2', (req, res/*, next*/) => { //what is this next thing in the get request?
+  const token = req.query.token;
+  const channelId = req.query.channelId;
+  console.log(token);
+  return res.json(channelDetailsV1(String(token), Number(channelId))); //might need to change channelDetails input
+                                                                           // to taken in tokens not numbers
+
+  // const data = req.query.echo as string;
+  // return res.json(echo(data));
+
+});
+
+app.post('channel/join/v2', (req, res) => {
+  const body = JSON.parse(req.body);
+  const token = body.token;
+  const channelId = body.channelId;
+  return res.json(channelJoinV1(token, channelId));
+  // return res.json(echo(data));
+   
+});
+
+//WRAPPING USERS FUNCTIONS - userProfileV1 (GET), user/profile/setname/v1 (PUT), user/profile/setemail/v1 (PUT),
+// user/profile/sethandle/v1 (PUT), users/all/v1 (GET)
+
+app.get('user/profile/v2', (req, res/*, next*/) => { //what is this next thing in the get request?
+  const token = req.query.token;
+  const uId = req.query.uId;
+  return res.json(userProfileV1(String(token), Number(uId))); //might need to change input
+                                                                           // to take in tokens not numbers
+});
+
+app.get('users/all/v1', (req, res/*, next*/) => { //what is this next thing in the get request?
+  const token = req.query.token;
+  return res.json(usersAllV1(String(token))); //might need to change input
+                                                                           // to take in tokens not numbers
+});
+
+app.put('user/profile/setname/v1', (req, res) => {
+  const body = JSON.parse(req.body);
+  const token = body.token;
+  const nameFirst = body.nameFirst;
+  const nameLast = body.nameLast;
+  
+  return res.json(userProfileSetNameV1(token, nameFirst, nameLast));
+});
+
+app.put('user/profile/setemail/v1', (req, res) => {
+  const body = JSON.parse(req.body);
+  const token = body.token;
+  const email = body.email;
+  
+  return res.json(userProfileSetEmailV1(token, email));
+});
+
+app.put('user/profile/sethandle/v1', (req, res) => {
+  const body = JSON.parse(req.body);
+  const token = body.token;
+  const handleStr = body.handleStr;
+  
+  return res.json(userProfileSetHandleV1(token, handleStr));
+});
+
+
+
+
+
+
+
 
 // for logging errors
 app.use(morgan('dev'));
